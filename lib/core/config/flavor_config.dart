a52d74719c26e891e49c506ddea9f20515e7840b
NISHANT-KC <nishant1938@gmail.com>
2026-06-10 09:10:07 +0530

    fix: use this.name in FlavorConfig extension, add null guard to appFlavor

--- lib/core/config/flavor_config.dart
+++ lib/core/config/flavor_config.dart
@@ -4,10 +4,14 @@ enum Flavor { dev, staging, prod }
 
 extension AppFlavorConfig on FlavorConfig {
   Flavor get appFlavor => Flavor.values.firstWhere(
-        (f) => f.name == FlavorConfig.instance.name,
+        (f) => f.name == name,
+        orElse: () => throw StateError(
+          'FlavorConfig.name "$name" does not match any Flavor. '
+          'Call FlavorConfig(name: "dev"|"staging"|"prod", ...) before use.',
+        ),
       );
 
-  bool get isDev => FlavorConfig.instance.name == 'dev';
-  bool get isStaging => FlavorConfig.instance.name == 'staging';
-  bool get isProduction => FlavorConfig.instance.name == 'prod';
+  bool get isDev => name == 'dev';
+  bool get isStaging => name == 'staging';
+  bool get isProduction => name == 'prod';
 }
