eb2ae0c0269e49cf57721799cc1007a02a336689
NidhiBingo <nidhi@bingosg.com>
2026-06-17 18:28:03 +0530

    feat(dashboard): create dashboard UI and reusable widgets
    - Developed the Dashboard screen UI
    
    - Implemented responsive design using the Sizer package
    
    - Created a reusable Search Bar widget
    
    - Developed a reusable Bottom Navigation Bar widget
    
    - Built reusable UI components to improve code reusability and maintainability
    
    - Integrated reusable widgets into the Dashboard screen
    
    Ref: CU-86d3cqg99

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,62 @@
+import 'package:sizer/sizer.dart';
+
+class AppSizes {
+  AppSizes._();
+
+  // Radius
+  static const double radiusXs = 6;
+  static const double radiusSm = 8;
+  static const double radiusMd = 12;
+  static const double radiusLg = 16;
+  static const double radiusXl = 14;
+  static const double radius2Xl = 24;
+
+  // Padding
+  static const double paddingXs = 8;
+  static const double paddingSm = 12;
+  static const double paddingMd = 16;
+  static const double paddingLg = 24;
+  static const double paddingXl = 32;
+
+  // Elevation / Shadow
+  static const double shadowBlurSm = 8;
+  static const double shadowBlurMd = 16;
+  static const double shadowBlurLg = 24;
+
+  // Button
+  static double buttonHeight = 7.h;
+  static double buttonRadius = radiusXl;
+
+  // TextField
+  static double textFieldRadius = radiusLg;
+
+  // OTP
+  static double otpBoxWidth = 13.w;
+  static double otpBoxHeight = 7.h;
+
+  // Card
+  static double cardRadius = radius2Xl;
+
+  // Icons
+  static double iconSm = 16.sp;
+  static double iconMd = 18.sp;
+  static double iconLg = 24.sp;
+  static double iconXl = 32.sp;
+
+  // Typography
+  static double heading = 22.sp;
+  static double title = 16.sp;
+  static double subTitle = 14.sp;
+  static double body = 13.sp;
+  static double caption = 11.sp;
+
+  // Logo
+  static double logoSize = 13.h;
+
+  // Auth Screen
+  static double authHeaderSpacing = 3.h;
+  static double authSectionSpacing = 2.h;
+
+  // AppBar
+  static double appBarHeight = 8.h;
+}
