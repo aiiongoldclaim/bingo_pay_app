084c963ae1086708add89cbf874305b6280faada
NISHANT-KC <nishant1938@gmail.com>
2026-08-05 17:23:02 +0530

    sentry application in progress

--- test/helpers/mocks.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class MockFlutterSecureStorage extends Mock implements FlutterSecureStorage {}
-class MockSharedPreferences extends Mock implements SharedPreferences {}
